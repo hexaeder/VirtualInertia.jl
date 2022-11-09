a0604107d09073a22564b43f786e213e83c2caed
Hans Würfel <git@wuerfel.io>
2022-11-09 10:28:08 +0100

    adopt swing, does not work though

--- examples/rts96.jl
+++ examples/rts96.jl
@@ -20,8 +20,6 @@ ENV["JULIA_DEBUG"] = ""
 
 BlockSystems.WARN_SIMPLIFY[] = false
 
-# se = VirtualInertia.SwingEquation(; D=0.1, u_mag=1, ω0=2π*50)
-
 #####
 ##### Import Network
 #####
@@ -33,28 +31,35 @@ edges_df = describe_edges(network)
 ##### Line Model
 #####
 lineblk = BSPiLine(; B_src=:B, B_dst=:B)
-staticpiline = StaticEdge(lineblk, [:R, :X, :B])
+staticpiline = StaticEdge(lineblk, [:R, :X, :B]);
 
 #####
 ##### Inverter Model
 #####
-inner = PerfectSource()
-outer = DroopControl(;V_ref=1, ω_ref=0, τ_P=:τ, τ_Q=:τ, K_P=:K, K_Q=:K)
-# here i've renamed K_Q and K_P in a way that they are the same
-
-invblk = Inverter(inner, outer; name=:inv)
-# we can add a load to the voltage source
-invloadblk = VSwithLoad(invblk)
-# invloadblk = transform_explicit_algebraic_outputs(invloadblk; verbose=true)
-inverter = ODEVertex(invloadblk, [:P_load, :Q_load, :P_ref, :Q_ref, :τ, :K])
+SWING = true
+generator = if !SWING
+    inner = PerfectSource()
+    # inner = PT1Source()
+    outer = DroopControl(;V_ref=1, ω_ref=0, τ_P=:τ, τ_Q=:τ, K_P=:K, K_Q=:K)
+    # here i've renamed K_Q and K_P in a way that they are the same
+    invblk = Inverter(inner, outer; name=:inv)
+    # we can add a load to the voltage source
+    invloadblk = VSwithLoad(invblk)
+    # invloadblk = transform_explicit_algebraic_outputs(invloadblk; verbose=true)
+    inverter = ODEVertex(invloadblk, [:P_load, :Q_load, :P_ref, :Q_ref, :τ, :K])
+else
+    swingblk = VirtualInertia.SwingEquation(; ω0=2π*50, u_mag=1)
+    swingloadblk = VSwithLoad(swingblk)
+    swing = ODEVertex(swingloadblk, [:P_load, :Q_load, :P_ref, :H, :D]);
+end
 
 #####
 ##### Load Model
 #####
-loadblk = PT1Load(τ=0.001)
+loadblk = PT1Load(τ=0.01)
 # loadblk = ConstLoad()
-# loadblk = PT1Load(EMT=true, τ=0.001, C=1e-4, ω0=2π*50)
-# loadblk = ConstLoad(EMT=true, C=1e-4, ω0=2π*50)
+# loadblk = PT1Load(EMT=true, τ=0.01, C=0.01, ω0=2π*50)
+# loadblk = ConstLoad(EMT=true, C=0.010, ω0=2π*50)
 load = ODEVertex(loadblk, [:P_load, :Q_load])
 
 #####
@@ -72,13 +77,23 @@ gens = filter(n -> n.type ∈ (:gen, :syncon), nodes_df)
 
 τinv = 0.01
 Kinv = 0.1
+Dswing = 0.1
 
-gen_para = collect(zip(ustrip.(u"pu", gens.P_load),
-                       ustrip.(u"pu", gens.Q_load),
-                       ustrip.(u"pu", gens.P_inj),
-                       ustrip.(u"pu", gens.Q_inj),
-                       Iterators.cycle(τinv),
-                       Iterators.cycle(Kinv)))
+gen_para = if !SWING
+    collect(zip(ustrip.(u"pu", gens.P_load),
+                ustrip.(u"pu", gens.Q_load),
+                ustrip.(u"pu", gens.P_inj),
+                ustrip.(u"pu", gens.Q_inj),
+                Iterators.cycle(τinv),
+                Iterators.cycle(Kinv)))
+else
+    collect(zip(ustrip.(u"pu", gens.P_load),
+                ustrip.(u"pu", gens.Q_load),
+                ustrip.(u"pu", gens.P_inj),
+                ustrip.(u"s", gens.H),
+                Iterators.cycle(Dswing),
+                Iterators.cycle(NaN)))
+end
 
 # loadsPload, Qload for loads
 loads = filter(n -> n.type == :load, nodes_df)
@@ -99,8 +114,8 @@ params = (nodeP, edgeP)
 #####
 ##### Crate NetworkDynamics model
 #####
-vertices = Vector{ODEVertex}(undef, length(nodes_df.n))
-vertices[gens.n] .= Ref(inverter);
+vertices = Vector{ODEVertex}(undef, length(nodes_df.n));
+vertices[gens.n] .= Ref(generator);
 vertices[loads.n] .= Ref(load);
 
 # solve the steady state problem
@@ -117,7 +132,7 @@ largest = findmax(x -> ismissing(x) ? 0 : x, nodes_df.P_inj)[2]
 disturbed = 10
 disturbance = -0.1
 
-tspan = (0, 7)
+tspan = (0, 1)
 prob = ODEProblem(nd, u0, tspan, params)
 precord = VirtualInertia.PRecord(prob)
 
@@ -128,7 +143,7 @@ function affect(integrator)
     largestp = newp[1][largest] |> collect
     largestp[3] -= disturbance # add this to p inj
     newp[1][disturbed] = Tuple(distp)
-    newp[1][largest] = Tuple(largestp)
+    # newp[1][largest] = Tuple(largestp)
 
     integrator.p = (newp[1], newp[2])
     record!(precord, integrator)
@@ -137,5 +152,4 @@ end
 cb = PresetTimeCallback(0.1, affect)
 GLMakie.closeall()
 sol = solve(prob, Rodas4(); dtmax=(prob.tspan[2]-prob.tspan[1])/1000, callback=cb);
-
 fig = inspect_solution(sol, network, precord)
